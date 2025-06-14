ae95cbf94e29d8e30fd3ac981d4d4c334e02615e
Tran Vu Lam <lam261204@gmail.com>
2025-06-14 12:01:55 +0700

    updated break line

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_10_074747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index [ "email" ], name: "index_users_on_email", unique: true
   end
 end
