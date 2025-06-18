243d529300b8e1813218cc697d2649c581b1afaa
Tran Vu Lam <lam261204@gmail.com>
2025-06-18 11:26:54 +0700

    feat: done chapter11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,11 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 10)
+  end
+
+  def show
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
@@ -16,9 +20,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       flash[:danger] = "Sign up error!!!"
       render "new", status: STATUS_UNPROCESSABLE_ENTITY
