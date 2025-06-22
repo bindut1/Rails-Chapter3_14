fdc44ddc0eae1216218821c87b362cadf8cc8da0
Tran Vu Lam <lam261204@gmail.com>
2025-06-22 13:04:52 +0700

    feat: config and integrated Cloudinary

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,6 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
-    @micropost.image.attach(params[:micropost][:image])
     if @micropost.save
       flash[:success] = t("microposts.create.success")
       redirect_to root_url
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -13,7 +13,4 @@ class Micropost < ApplicationRecord
       less_than: 5.megabytes,
       message: "should be less than 5MB"
     }
-  def display_image
-    image.variant(resize_to_limit: [ 500, 500 ])
-  end
 end
